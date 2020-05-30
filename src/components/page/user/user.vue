209524764738169ffc71f8a28b047cef48c53d7b
xiaowen1024 <15214440324@163.com>
2020-05-30 19:06:56 +0800

    通知

--- src/components/page/user/user.vue
+++ src/components/page/user/user.vue
@@ -61,6 +61,12 @@
                                 @click="upd(scope.row)"
                         >编辑
                         </el-button>
+                        <el-button
+                                type="text"
+                                icon="el-icon-edit"
+                                @click="inform(scope.row)"
+                        >通知
+                        </el-button>
                         <el-popconfirm
                                 title="确认禁用此用户吗？"
                                 @onConfirm="handleDelete(scope.row)"
@@ -164,6 +170,86 @@
             </div>
         </el-dialog>
 
+        <!--通知-->
+        <el-dialog title="添加通知" :visible.sync="dialogInfo"
+                   :close-on-click-modal="closeOnClickModal">
+            <el-button
+                    type="text"
+                    style="margin-bottom: 1%"
+                    @click="informAdd"
+            >添加
+            </el-button>
+            <el-table
+                    :data="tableData1"
+                    border
+                    class="table"
+                    ref="multipleTable"
+                    v-loading="$store.state.requestLoading"
+            >
+                <el-table-column :show-overflow-tooltip="true" width="130" prop="introduce" label="图片" align="center">
+                    <template slot-scope="scope">
+                        <img :src="scope.row.image" width="70" height="70"/>
+                    </template>
+                </el-table-column>
+                <el-table-column prop="title" label="标题" align="center"
+                                 :show-overflow-tooltip="true"></el-table-column>
+                <el-table-column prop="content" label="内容" align="center"
+                                 :show-overflow-tooltip="true"></el-table-column>
+                <el-table-column prop="createTime" label="创建时间" align="center" :formatter="formatDate"
+                                 :show-overflow-tooltip="true"></el-table-column>
+            </el-table>
+            <div class="pagination">
+                <el-pagination
+                        @size-change="handleSizeChange1"
+                        @current-change="handleCurrentChange1"
+                        :current-page="currentPage1"
+                        :page-sizes="pageSizes1"
+                        :page-size="PageSize1"
+                        layout="total, sizes, prev, pager, next, jumper"
+                        :total="totalCount1">
+                </el-pagination>
+            </div>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogInfo = false">取 消</el-button>
+                <el-button type="primary" @click="dialogInfo = false">确 定</el-button>
+            </div>
+        </el-dialog>
+
+
+        <!--添加通知-->
+        <el-dialog title="添加通知" :visible.sync="dialogInfoadd"
+                   :close-on-click-modal="closeOnClickModal">
+            <el-form ref="courseform" :model="formInfo" :rules="rules1">
+                <el-row>
+                    <el-col :span="12" style="border-right: 1px #666666 solid">
+                        <div class="grid-content bg-purple">
+                            <el-form-item label="标题：" prop="title">
+                                <el-input style="width: 300px" v-model="formInfo.title" size="mini"
+                                          class="inputform"></el-input>
+                            </el-form-item>
+                            <el-form-item label="内容：" prop="content">
+                                <el-input style="width: 300px" v-model="formInfo.content" size="mini"
+                                          class="inputform"></el-input>
+                            </el-form-item>
+                        </div>
+                    </el-col>
+                    <el-col :span="12">
+                        <el-form-item style="margin-left:10%;width: 300px" label="图片">
+                            <upLoad id-name="team1"
+                                    :fileList="fileList1"
+                                    :onUpLoadSuccess="onUpLoadSuccess1"
+                                    :onUpLoadError="onUpLoadError1"
+                                    :filesNumber="1"
+                                    :showFileList="true"></upLoad>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogInfoadd = false">取 消</el-button>
+                <el-button type="primary" @click="dialogInfoSave('form')">确 定</el-button>
+            </div>
+        </el-dialog>
 
     </div>
 </template>
@@ -177,7 +263,10 @@
         components: {upLoad},
         data() {
             return {
+                dialogInfo: false,
+                dialogInfoadd: false,
                 fileList: [],
+                fileList1: [],
                 dialogFormVisible: false,
                 form: {
                     id: '',
@@ -191,7 +280,13 @@
                     birthday: '',
                     status: 'yes',
                 },
-
+                formInfo: {
+                    userId: '',
+                    content: '',
+                    title: '',
+                    image: '',
+                },
+                row:'',
                 loading: true,
                 address: false,
                 addressData: [],
@@ -208,6 +303,18 @@
                 pageSizes: [10, 20, 50, 100],
                 // 默认每页显示的条数（可修改）
                 PageSize: 10,
+
+                // 总数据
+                tableData1: [],
+                // 默认显示第几页
+                currentPage1: 1,
+                // 总条数，根据接口获取数据长度(注意：这里不能为空)
+                totalCount1: 0,
+                // 个数选择器（可修改）
+                pageSizes1: [10, 20, 50, 100],
+                // 默认每页显示的条数（可修改）
+                PageSize1: 10,
+
                 s_name: "",
                 s_phone: "",
                 s_value: "",
@@ -225,6 +332,16 @@
                     ],
                 },
                 options: [],
+
+
+                rules1: {
+                    title: [
+                        {required: true, message: "请输入标题", trigger: "blur"}
+                    ],
+                    content: [
+                        {required: true, message: "请输内容", trigger: "blur"}
+                    ],
+                },
             };
         },
         created() {
@@ -232,6 +349,21 @@
             this.getSelect();
         },
         methods: {
+            inform(row) {
+                this.fileList1 = [];
+                this.formInfo = {
+                    userId: row.id,
+                    content: '',
+                    title: '',
+                    image: '',
+                };
+                this.row=row;
+                this.getInform(row);
+                this.dialogInfo = true;
+            },
+            informAdd() {
+                this.dialogInfoadd = true;
+            },
             // 每页显示的条数
             handleSizeChange(val) {
                 // 改变每页显示的条数
@@ -249,7 +381,22 @@
                 this.getData(this.PageSize, (val) * (this.pageSize))
             },
 
-            // 获取 easy-mock 的模拟数据
+            // 每页显示的条数
+            handleSizeChange1(val) {
+                // 改变每页显示的条数
+                this.PageSize1 = val
+                // 点击每页显示的条数时，显示第一页
+                this.getInform(this.row)
+                // 注意：在改变每页显示的条数时，要将页码显示到第一页
+                this.currentPage1 = 1
+            },
+            // 显示第几页
+            handleCurrentChange1(val) {
+                // 改变默认的页数
+                this.currentPage1 = val
+                // 切换页码时，要获取每页显示的条数
+                this.getInform(this.row)
+            },
             getData() {
                 this.loading = true;
                 this.$axios.post("/backUser/query", {
@@ -264,6 +411,19 @@
                 });
                 this.loading = false;
             },
+            getInform(row) {
+                this.loading = true;
+                this.$axios.post("/inform/query", {
+                    pageNo: this.currentPage1,
+                    pageSize: this.PageSize1,
+                    userId: row.id,
+                }).then(res => {
+                    this.tableData1 = res.data.records;
+                    this.totalCount1 = res.data.total;
+                });
+                this.loading = false;
+                this.dialogInfo = true;
+            },
             getSelect() {
                 this.$axios.post("/organ/queryByWechatId", {}).then(res => {
                     this.options = res.data;
@@ -277,7 +437,6 @@
              * 上传成功后的回调
              */
             onUpLoadSuccess(uploadUrl) {
-                console.log(uploadUrl)
                 this.fileList = [];
                 this.fileList.push({name: uploadUrl, url: uploadUrl});
                 this.form.head = uploadUrl;
@@ -287,6 +446,22 @@
              */
             onUpLoadError(err) {
                 console.log(err)
+                this.$message.error('上传失败')
+            },
+
+            /**
+             * 上传成功后的回调
+             */
+            onUpLoadSuccess1(uploadUrl) {
+                console.log(uploadUrl)
+                this.fileList1 = [];
+                this.fileList1.push({name: uploadUrl, url: uploadUrl});
+                this.formInfo.image = uploadUrl;
+            },
+            /**
+             * 上传失败后的回调
+             */
+            onUpLoadError1(err) {
                 this.$message.error('上传失败')
             },
             formatDate(row) {
@@ -320,6 +495,31 @@
                 this.fileList.push({name: row.head, url: row.head});
                 this.form = row;
                 this.dialogFormVisible = true;
+            },
+            dialogInfoSave(formName) {
+                if (this.formInfo.image == '' || this.formInfo.image == null) {
+                    this.$message.error("未添加图片");
+                    return false;
+                }
+                this.$refs.courseform.validate(valid => {
+                    if (valid) {
+                        let fd = JSON.parse(JSON.stringify(this.formInfo));
+                        this.$axios.post('/inform/add', fd).then(res => {
+                            if (!res.success) {
+                                this.$message.error(res.errMsg);
+                                return;
+                            }
+                            this.$message.success(`操作成功`);
+                            this.getInform(this.row);
+                            this.dialogInfoadd = false;
+                        });
+                    } else {
+                        console.error('error submit!!');
+                        return false;
+                    }
+                });
+
+
             },
             saveEdit(formName) {
                 if (this.form.head == '' || this.form.head == null) {
@@ -362,7 +562,7 @@
             },
             handleDelete(row) {
                 /* 更新 */
-                this.$axios.post('/backUser/delete',{id:row.id} ).then(res => {
+                this.$axios.post('/backUser/delete', {id: row.id}).then(res => {
                     if (!res.success) {
                         this.$message.error(res.errMsg);
                         return;
